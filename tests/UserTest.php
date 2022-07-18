de99d9078d762828b0c8ec7828ed264004425afb
Flavien Chagras <flavien.chagras@protonmail.com>
2022-07-18 10:53:31 +0200

    tests(app): tests done

--- tests/UserTest.php
+++ tests/UserTest.php
@@ -53,6 +53,79 @@ class UserTest extends TestCase {
         $this->seeStatusCode(422);
     }
 
+    public function test_can_update_user(): void {
+        $user = User::factory()->create();
+
+        $this->post(route('users.login'), [
+            'email' => $user->email,
+            'password' => 'root',
+            'recaptcha' => 'test'
+        ]);
+
+        $user2 = User::factory()->make();
+
+        $this->post(route('users.edit'), [
+            'lastname' => $user2->lastname,
+            'firstname' => $user2->firstname,
+        ], ['Authorization' => "Bearer ". json_decode($this->response->getContent())->data->token]);
+
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'data' => null,
+            'meta' => [
+                'success' => true,
+                'message' => 'Successfully edited user'
+            ]
+        ]);
+
+        $this->seeInDatabase('users', [
+            'id' => $user->id,
+            'email' => $user->email,
+            'firstname' => $user2->firstname,
+            'lastname' => $user2->lastname
+        ]);
+    }
+
+    public function test_cannot_update_user(): void {
+        $user = User::factory()->make();
+
+        $this->post(route('users.edit'), [
+            'lastname' => $user->lastname,
+            'firstname' => $user->firstname,
+        ]);
+
+        $this->seeStatusCode(401);
+        $this->seeJson([
+            'data' => 'Token not provided',
+            'meta' => [
+                'success' => false,
+                'message' => 'Unauthorized'
+            ]
+        ]);
+    }
+
+    public function test_can_delete_user(): void {
+        $user = User::factory()->create();
+
+        $this->post(route('users.login'), [
+            'email' => $user->email,
+            'password' => 'root',
+            'recaptcha' => 'test'
+        ]);
+
+        $this->delete(route('users.delete'), [], ['Authorization' => "Bearer ". json_decode($this->response->getContent())->data->token]);
+
+        $this->seeStatusCode(204);
+
+        $this->notSeeInDatabase('users', [
+            'id' => $user->id,
+            'email' => $user->email,
+            'firstname' => $user->firstname,
+            'lastname' => $user->lastname,
+            'deleted_at' => null
+        ]);
+    }
+
     public function test_can_login(): void {
         $user = User::factory()->create();
 
@@ -96,29 +169,8 @@ class UserTest extends TestCase {
     }
 
     public function test_can_refresh_token(): void {
-        $user = User::factory()->create();
-
-        $this->post(route('users.login'), [
-            'email' => $user->email,
-            'password' => 'root',
-            'recaptcha' => 'test'
-        ]);
-
-        $this->seeStatusCode(200);
-        $this->seeJsonStructure([
-            'data' => [
-                'token',
-                'token_type',
-                'expires_in',
-            ],
-            'meta' => [
-                'success',
-                'message'
-            ]
-        ]);
-
-        $this->post(route('users.refresh'), [
-            'token' => json_decode($this->response->getContent())->data->token
+        $this->post(route('users.refresh'), [], [
+            'Authorization' => 'Bearer ' . $this->token()
         ]);
 
         $this->seeStatusCode(200);
@@ -136,29 +188,8 @@ class UserTest extends TestCase {
     }
 
     public function test_can_logout(): void {
-        $user = User::factory()->create();
-
-        $this->post(route('users.login'), [
-            'email' => $user->email,
-            'password' => 'root',
-            'recaptcha' => 'test'
-        ]);
-
-        $this->seeStatusCode(200);
-        $this->seeJsonStructure([
-            'data' => [
-                'token',
-                'token_type',
-                'expires_in',
-            ],
-            'meta' => [
-                'success',
-                'message'
-            ]
-        ]);
-
-        $this->post(route('users.logout'), [
-            'token' => json_decode($this->response->getContent())->data->token
+        $this->post(route('users.logout'), [], [
+            'Authorization' => 'Bearer ' . $this->token()
         ]);
 
         $this->seeStatusCode(200);
@@ -171,27 +202,122 @@ class UserTest extends TestCase {
         ]);
     }
 
-//    public function test_can_get_user(): void {
-//        $user = User::factory()->create();
-//
-//        $this->get(route('users.show', $user->id));
-//        $this->seeStatusCode(200);
-//        $this->seeJsonStructure([
-//            'data' => [
-//                'email',
-//                'firstname',
-//                'lastname',
-//                'id'
-//            ],
-//            'meta' => [
-//                'success',
-//                'message'
-//            ]
-//        ]);
-//    }
-//
-//    public function test_cannot_get_user(): void {
-//        $this->get(route('users.show', 'wrong'));
-//        $this->seeStatusCode(404);
-//    }
+    public function test_can_get_me(): void {
+        $this->get(route('users.me'), ['Authorization' => 'Bearer '. $this->token()]);
+        $this->seeStatusCode(200);
+        $this->seeJsonStructure([
+            'data' => [
+                'email',
+                'firstname',
+                'lastname',
+                'id'
+            ],
+            'meta' => [
+                'success',
+                'message'
+            ]
+        ]);
+    }
+
+    public function test_can_forgot_password(): void {
+        $user = User::factory()->create();
+        $this->post(route('users.forgot'), [
+            'email' => $user->email,
+            'recaptcha' => 'test'
+        ]);
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'data' => null,
+            'meta' => [
+                'success'=> true,
+                'message' => 'Request Sent !'
+            ]
+        ]);
+        $this->notseeInDatabase('users', [
+            'id' => $user->id,
+            'reset_password' => null
+        ]);
+    }
+
+    public function test_can_verify_token(): void {
+        $user = User::factory()->create([
+            'reset_password' => 'test'
+        ]);
+
+        $this->get(route('users.reset', ['reset_password' => $user->reset_password]));
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'data' => [
+                'id' => $user->id,
+                'email' => $user->email,
+                'firstname' => $user->firstname,
+                'lastname' => $user->lastname,
+            ],
+            'meta' => [
+                'success'=> true,
+                'message' => 'Token OK'
+            ]
+        ]);
+    }
+
+    public function test_can_reset_password(): void {
+        $user = User::factory()->create([
+            'reset_password' => 'test'
+        ]);
+
+        $this->post(route('users.reset'), [
+            'reset_password' => $user->reset_password,
+            'password' => 'rootroot',
+            'password_confirmation' => 'rootroot',
+        ]);
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'data' => null,
+            'meta' => [
+                'success'=> true,
+                'message' => 'Successfully edited password'
+            ]
+        ]);
+
+        $this->seeInDatabase('users', [
+            'id' => $user->id,
+            'reset_password' => null
+        ]);
+
+        $this->post(route('users.login'), [
+            'email' => $user->email,
+            'password' => 'rootroot',
+            'recaptcha' => 'test'
+        ]);
+        $this->seeStatusCode(200);
+    }
+
+    public function test_can_update_password(): void {
+        $user = User::factory()->create();
+        $this->post(route('users.login'), [
+            'email' => $user->email,
+            'password' => 'root',
+            'recaptcha' => 'test'
+        ]);
+
+        $this->post(route('users.password'), [
+            'password' => 'rootroot',
+            'password_confirmation' => 'rootroot',
+        ], ['Authorization' => "Bearer ". json_decode($this->response->getContent())->data->token]);
+        $this->seeStatusCode(200);
+        $this->seeJson([
+            'data' => null,
+            'meta' => [
+                'success'=> true,
+                'message' => 'Successfully edited password'
+            ]
+        ]);
+
+        $this->post(route('users.login'), [
+            'email' => $user->email,
+            'password' => 'rootroot',
+            'recaptcha' => 'test'
+        ]);
+        $this->seeStatusCode(200);
+    }
 }
